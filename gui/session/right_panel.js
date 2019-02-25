5a374aaed74ad15d20e369431a13924b6a8d2c76
“Monder87” <frankzocca@gmail.com>
2019-02-25 22:15:34 +0800

    building the right panel for economy entities

--- gui/session/right_panel.js
+++ gui/session/right_panel.js
@@ -1,13 +1,31 @@
-function openSlave() {
-  Engine.GetGUIObjectByName("panelContainer").hidden = false;
+//function openSlave() {
+//  Engine.GetGUIObjectByName("panelContainer").hidden = false;
+//}
+
+//function closeSlave() {
+//  Engine.GetGUIObjectByName("panelContainer").hidden = true;
+//}
+
+//function toggleSlave() {
+//  Engine.GetGUIObjectByName(
+//    "panelContainer"
+//  ).hidden = !Engine.GetGUIObjectByName("panelContainer").hidden;
+//}
+
+function openCommand() {
+  Engine.GetGUIObjectByName("panelContainerCommand").hidden = false;
+  Engine.GetGUIObjectByName("panelContainerConsume").hidden = true;
+  Engine.GetGUIObjectByName("panelContainerProduce").hidden = true;
 }
 
-function closeSlave() {
-  Engine.GetGUIObjectByName("panelContainer").hidden = true;
+function openConsume() {
+  Engine.GetGUIObjectByName("panelContainerCommand").hidden = true;
+  Engine.GetGUIObjectByName("panelContainerConsume").hidden = false;
+  Engine.GetGUIObjectByName("panelContainerProduce").hidden = true;
 }
 
-function toggleSlave() {
-  Engine.GetGUIObjectByName(
-    "panelContainer"
-  ).hidden = !Engine.GetGUIObjectByName("panelContainer").hidden;
+function openProduce() {
+  Engine.GetGUIObjectByName("panelContainerCommand").hidden = true;
+  Engine.GetGUIObjectByName("panelContainerConsume").hidden = true;
+  Engine.GetGUIObjectByName("panelContainerProduce").hidden = false;
 }
