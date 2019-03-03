fabdac6e91fc05b4d2278786a2048e69583b81e7
francesco <francesco@francescos-MacBook-Air.local>
2019-03-03 17:45:48 +0800

    adding the happiness face on top of 3d character, so to easy reconize the civ economic status

--- simulation/components/GuiInterface.js
+++ simulation/components/GuiInterface.js
@@ -377,7 +377,8 @@ GuiInterface.prototype.GetEntityState = function(player, ent) {
   if (cmpEntityConsumer)
     ret.entityConsumer = {
       maxCapac: cmpEntityConsumer.GetProductCapacities(),
-      carring2: cmpEntityConsumer.GetProductCarring()
+      carring2: cmpEntityConsumer.GetProductCarring(),
+      ent: ent
     };
 
   ret.canGarrison = !!Engine.QueryInterface(ent, IID_Garrisonable);
@@ -889,6 +890,29 @@ GuiInterface.prototype.GetAvailableStock = function(ent) {
     "special/stock/jewelry",
     "special/stock/slave"
   ];
+};
+
+GuiInterface.prototype.AddHappinessUnit = function(player, ent) {
+  //error(ent);
+  const g_NaturalColor = "255 255 255 255";
+  let cmpOverlayRenderer = Engine.QueryInterface(ent, IID_OverlayRenderer);
+  cmpOverlayRenderer.Reset();
+  let width = 1;
+  let height = 1;
+  let offset = { x: 0, y: 5, z: 0 };
+  let yoffset = 0.1;
+  cmpOverlayRenderer.AddSprite(
+    "art/textures/ui/session/icons/happy.png",
+    { x: -width / 2, y: yoffset },
+    { x: width / 2, y: height + yoffset },
+    offset,
+
+    //  "art/textures/ui/session/icons/happy.png",
+    //  { x: -3 / 2, y: yoffset },
+    //  { x: -3 / 2, y: -3 + yoffset },
+    //  { x: 3, y: 10, z: 0 },
+    g_NaturalColor
+  );
 };
 
 // === Economy Functions End == \\
@@ -2145,6 +2169,7 @@ let exposedFunctions = {
   GetAllConsumingProducts: 1,
   RightPanelEnabled: 1,
   RightPanelFocused: 1,
+  AddHappinessUnit: 1,
   // === End == \\
   SetStatusBars: 1,
   GetPlayerEntities: 1,
