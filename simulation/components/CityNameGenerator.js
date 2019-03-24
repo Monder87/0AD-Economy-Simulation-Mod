eff6c10a9108b630af9c9296b44930b2c4cfae47
Monder87 <frankzocca@gmail.com>
2019-03-25 01:05:50 +0800

    addedd the kush cityname list to cityname comonent

--- simulation/components/CityNameGenerator.js
+++ simulation/components/CityNameGenerator.js
@@ -17,14 +17,36 @@ CityNameGenerator.prototype.Init = function() {
     "Hadrumetum",
     "Zama Regia"
   ];
-
   this.cartCityCounter = 0;
   this.cartCityCounter2 = 1;
+  // kush
+  this.kushCities = [
+    "Napata﻿",
+    "Meroë﻿",
+    "Naqa",
+    "Kawa",
+    "Tabo",
+    "Sanam",
+    "Dangeil",
+    "Basa",
+    "Sedeinga",
+    "Sonijat ",
+    "Muweis",
+    "Karanog",
+    "Dakka",
+    "Hamadab﻿﻿",
+    "Amara﻿ East"
+  ];
+  this.kushCityCounter = 0;
+  this.kushCityCounter2 = 1;
 };
 
 CityNameGenerator.prototype.GenerateCityName = function(civ) {
-  if (this.cartCityCounter % 10 == 0) {
+  if (this.cartCityCounter % this.cartCities.length == 0) {
     this.cartCityCounter2 += 1;
+  }
+  if (this.kushCityCounter % this.kushCities.length == 0) {
+    this.kushCityCounter2 += 1;
   }
   if (civ == "cart") {
     // we choose first
@@ -36,8 +58,14 @@ CityNameGenerator.prototype.GenerateCityName = function(civ) {
     this.cartCities.push(`${city2}${this.cartCityCounter2}`);
     // we addd the counter
     this.cartCityCounter += 1;
-
     // we return th name
+    return city;
+  } else if (civ == "kush") {
+    let city = this.kushCities[0];
+    this.kushCities.shift();
+    let city2 = city.replace(/[0-9]/g, "");
+    this.kushCities.push(`${city2}${this.kushCityCounter2}`);
+    this.kushCityCounter += 1;
     return city;
   }
 };
